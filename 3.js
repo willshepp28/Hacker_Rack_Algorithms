8b375dfaba64c2e04ce1b8900ff2f793a7f10fa6
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-02-25 16:08:23 -0500

    completed new challenge

---
+++ 3.js
@@ -0,0 +1,46 @@
+/**
+ * PROBLEM
+ * Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+Function Description
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+aVeryBigSum has the following parameter(s):
+
+ar: an array of integers .
+Input Format
+
+The first line of the input consists of an integer . 
+The next line contains  space-separated integers contained in the array.
+
+Output Format
+
+Print the integer sum of the elements in the array.
+ */
+
+
+function aVeryBigSum(ar) {
+    let sum;
+    
+    for(let i = 0; i < ar.length; i++) {  
+      if(i === 0){
+         sum = ar[0] + ar[1];
+         ar.splice(0,1);
+      } else {
+        sum = sum + ar[i];
+      }
+    }
+    
+    return sum;
+  }
+  
+  
+  aVeryBigSum([1000000001, 
+               1000000002,
+               1000000003,
+               1000000004,
+               1000000005]);
+
+  aVeryBigSum([10,10,10,10]);
+  aVeryBigSum([ 80, 5,10,9,9,9,7,55555]);
